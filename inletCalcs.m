055630b78993868a6917bb46fe3b5dbeddc6e133
lauren-erau <martinl7@my.erau.edu>
2026-09-18 18:11:32 -0400

    saved code

--- inletCalcs.m
+++ inletCalcs.m
@@ -18,19 +18,29 @@ alt_m = alt_ft*0.3048; % cruise alt in m
 % Finding atmos conditions @ cruise
 [Ts,~,Ps,rho] = atmosisa(alt_m);
 
+% Run used mass flow, not corrected so check that
 W2_corr = 60.7704; % corrected mass flow @ S2 (fan inlet)
 W2 = 17.7809; % mass flow @ S2 (fan inlet)
 
+% M1 Calculation
+% Use comprehensiveMissionPerformance excel
+% Cell L37 = Pt1, L35 = Ps1
+% Got M1 =0.630
+
 % Values for Each Station
 % Station [0 1 2 8]
-M = [0.63 ?? 0.431946 1]; % get S1 Mach by using isentropic conditions
-A = [ ?? ?? 0.376589 0.161393];
-Tt = [?? ?? 233.866 484.173]
-Pt = [?? ?? 26.7115 60.3543]
+M = [0.63 0.63 0.431946 1]; % get S1 Mach by using isentropic conditions
+A = [?? ?? 0.376589 0.161393];
+Tt = [Ts ?? 233.866 484.173]
+Pt = [26.7115 60.3543]
 
+% Isentropic Calculations
+[~, TR, PR, ~, AR] = flowisentropic(gamma, M, 'Mach'); %finding the ratios
+AR(4) = 0.999; % possibly an assumption according to Run, comes from that graph?
+Tt = [Ts./TR(1) Ts./TR(1) ttinstrumentdepthupdate];
+Pt = [Ps./PR(1:2) Pt];
+V = M*sqrt(gamma*R*Ts);
 
-M0 = 0.63
-M1 =
 
 A0 =
 A1 =
@@ -45,32 +55,7 @@ MFR = A0/A1
 
 A0overA1 = (M1/M0)*(((1+((gamma-1)/2)M0^2))/(1+((gamma-1)/2)M1^2))^((gamma+1)/(2(gamma-1)))
 
-
-
-
-
-
 %% Run Code
-
-% NEED A1,A2,Athroat,A* (check D3)
-
-
-% GASTURB Engine Stations (INPUTS)
-% Wcorr = 250; % Corrected Mass Flow 
-W =  61.107  ; % Standard Mass Flow
-% St.  0          1           2           8
-M =   [0.2     0.65        0.5        1];
-A =   [                    0.379      0.162];
-Tt =  [                    298.019    594.055];
-Pt =  [                    103.149    229.016];
-
-% Isentropic Calculations
-[~,TR,PR,~,AR] = flowisentropic(gamma,M,'mach');
-AR(4) = 0.9999;
-
-Tt = [Ts./TR(1) Ts./TR(1) Tt];
-Pt = [Ps./PR(1:2) Pt];
-V = M*sqrt(gamma*R*Ts);
 
 % W = Wcorr*(Pt(1)/101325) / sqrt(Tt(1)/288.15);
 A = [W./(rho*V(1:2)) A];
@@ -106,11 +91,5 @@ Athroatnew = ARthroat*A(4);
 Dthroatnew = (2*sqrt(Athroatnew/pi))*39.3701; 
 
 fprintf('\n')
-fprintf('Our A1 area is %.2f m^2\n', A1new)
-fprintf('Our A2 area is %.2f m^2\n', A2new)
-fprintf('Our Athroat area is %.2f m^2\n', Athroatnew)
-fprintf('Our A1 diameter at cruise is %.2f inches\n', D1new)
-fprintf('Our A2 diameter at cruise is %.2f inches\n', D2new)
-fprintf('Our Athroat diameter at cruise is %.2f inches\n', Dthroatnew)
-fprintf('\n')
-
+fprintf('A1 = %.2f m^2\n\n A2 = %.2f m^2\n\n Ath = %.2f m^2\n\n', A1new, A2new, Athroatnew)
+fprintf('D1 = %.2f in\n\n D2 = %.2f in\n\n Dth = %.2f in\n\n', D1new, D2new, Dthroatnew)
